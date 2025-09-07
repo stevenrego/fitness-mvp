97af133bba384e2f6603f664ecb6b944c01df58e
stevenrego <stevenrego@hotmail.com>
2025-09-07 16:03:43 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-﻿import type { Config } from 'tailwindcss';
+import type { Config } from 'tailwindcss';
+
 const config: Config = {
   content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
   theme: { extend: {} },
